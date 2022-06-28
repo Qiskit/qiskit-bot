74b2e8411b2a3650d87e291c8d6ea14589374d9d
Abby Mitchell <abby.s.mitchell@gmail.com>
2022-06-28 16:32:15 -0400

    :sparkles: lint :sparkles:

--- qiskit_bot/community.py
+++ qiskit_bot/community.py
@@ -14,17 +14,18 @@
 
 MONITORED_REPOS = ['qiskit-terra']
 
+
 def add_community_label(pr_data, repo):
     """Add community label to PR when author not associated with core team"""
     if repo.name in MONITORED_REPOS:
         # check if PR was authored by soemone outside core repo team
         if pr_data['pull_request']['author_association'] != 'MEMBER':
-                # fetch PR metadata
-                pr = repo.gh_repo.get_pull(pr_data['pull_request']['number'])
-                # tag PR with 'community PR' label & notify reviewers
-                labels = pr.get_labels()
-                label_names = [label['name'] for label in labels]
-                if "Community PR" not in label_names:
-                    pr.add_to_labels("Community PR")
-                    pr.create_review_request(
-                        team_reviewers=["community-reviewers"])
+            # fetch PR metadata
+            pr = repo.gh_repo.get_pull(pr_data['pull_request']['number'])
+            # tag PR with 'community PR' label & notify reviewers
+            labels = pr.get_labels()
+            label_names = [label['name'] for label in labels]
+            if "Community PR" not in label_names:
+                pr.add_to_labels("Community PR")
+                pr.create_review_request(
+                    team_reviewers=["community-reviewers"])
--- tests/test_community.py
+++ tests/test_community.py
@@ -18,12 +18,13 @@ import fixtures
 
 from qiskit_bot import community
 
+
 class TestCommunity(fixtures.TestWithFixtures, unittest.TestCase):
-    
+
     def setUp(self):
         self.temp_dir = fixtures.TempDir()
         self.useFixture(self.temp_dir)
-    
+
     @unittest.mock.patch("multiprocessing.Process")
     def test_basic_community_pr(self, sub_mock):
         repo = unittest.mock.MagicMock()
@@ -32,7 +33,8 @@ class TestCommunity(fixtures.TestWithFixtures, unittest.TestCase):
         repo.name = 'qiskit-terra'
         repo.gh_repo = gh_mock
         gh_mock.get_pull.return_value = pr_mock
-        pr_mock.get_labels.return_value = [{'name': 'test_label_1'}, {'name': 'test_label_2'}]
+        pr_mock.get_labels.return_value = [{
+            'name': 'test_label_1'}, {'name': 'test_label_2'}]
         data = {'pull_request': {'author_association': None, 'number': 1234}}
 
         community.add_community_label(data, repo)
@@ -41,7 +43,7 @@ class TestCommunity(fixtures.TestWithFixtures, unittest.TestCase):
         pr_mock.get_labels.assert_called_once()
         pr_mock.add_to_labels.assert_called_once()
         pr_mock.create_review_request.assert_called_once()
-    
+
     @unittest.mock.patch("multiprocessing.Process")
     def test_repo_not_monitored(self, sub_mock):
         repo = unittest.mock.MagicMock()
@@ -50,7 +52,8 @@ class TestCommunity(fixtures.TestWithFixtures, unittest.TestCase):
         repo.name = 'qiskit-nature'
         repo.gh_repo = gh_mock
         gh_mock.get_pull.return_value = pr_mock
-        pr_mock.get_labels.return_value = [{'name': 'test_label_1'}, {'name': 'test_label_2'}]
+        pr_mock.get_labels.return_value = [{
+            'name': 'test_label_1'}, {'name': 'test_label_2'}]
         data = {'pull_request': {'author_association': None, 'number': 1234}}
 
         community.add_community_label(data, repo)
@@ -59,7 +62,7 @@ class TestCommunity(fixtures.TestWithFixtures, unittest.TestCase):
         pr_mock.get_labels.assert_not_called()
         pr_mock.add_to_labels.assert_not_called()
         pr_mock.create_review_request.assert_not_called()
-    
+
     @unittest.mock.patch("multiprocessing.Process")
     def test_contributor_is_member(self, sub_mock):
         repo = unittest.mock.MagicMock()
@@ -68,8 +71,10 @@ class TestCommunity(fixtures.TestWithFixtures, unittest.TestCase):
         repo.name = 'qiskit-terra'
         repo.gh_repo = gh_mock
         gh_mock.get_pull.return_value = pr_mock
-        pr_mock.get_labels.return_value = [{'name': 'test_label_1'}, {'name': 'test_label_2'}]
-        data = {'pull_request': {'author_association': 'MEMBER', 'number': 1234}}
+        pr_mock.get_labels.return_value = [{
+            'name': 'test_label_1'}, {'name': 'test_label_2'}]
+        data = {
+            'pull_request': {'author_association': 'MEMBER', 'number': 1234}}
 
         community.add_community_label(data, repo)
 
@@ -77,7 +82,7 @@ class TestCommunity(fixtures.TestWithFixtures, unittest.TestCase):
         pr_mock.get_labels.assert_not_called()
         pr_mock.add_to_labels.assert_not_called()
         pr_mock.create_review_request.assert_not_called()
-    
+
     @unittest.mock.patch("multiprocessing.Process")
     def test_already_labelled(self, sub_mock):
         repo = unittest.mock.MagicMock()
@@ -86,7 +91,8 @@ class TestCommunity(fixtures.TestWithFixtures, unittest.TestCase):
         repo.name = 'qiskit-terra'
         repo.gh_repo = gh_mock
         gh_mock.get_pull.return_value = pr_mock
-        pr_mock.get_labels.return_value = [{'name': 'Community PR'}, {'name': 'test_label_2'}]
+        pr_mock.get_labels.return_value = [{
+            'name': 'Community PR'}, {'name': 'test_label_2'}]
         data = {'pull_request': {'author_association': None, 'number': 1234}}
 
         community.add_community_label(data, repo)
